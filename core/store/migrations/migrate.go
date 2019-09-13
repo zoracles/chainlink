d695bfb8af4ed97a1af75926449fdc8004b56cb9
Alex Coventry <alx@mit.edu>
2019-09-13 13:26:05 -0400

    Explicit db migration for new Encumbrance fields/columns

--- core/store/migrations/migrate.go
+++ core/store/migrations/migrate.go
@@ -23,6 +23,7 @@ import (
 	"github.com/smartcontractkit/chainlink/core/store/migrations/migration1566915476"
 	"github.com/smartcontractkit/chainlink/core/store/migrations/migration1567029116"
 	"github.com/smartcontractkit/chainlink/core/store/migrations/migration1568280052"
+	"github.com/smartcontractkit/chainlink/core/store/migrations/migration1568390387"
 	"github.com/smartcontractkit/chainlink/core/store/migrations/migration1568833756"
 	"github.com/smartcontractkit/chainlink/core/store/migrations/migration1570087128"
 	gormigrate "gopkg.in/gormigrate.v1"
@@ -114,6 +115,8 @@ func Migrate(db *gorm.DB) error {
 		{
 			ID:      "1570087128",
 			Migrate: migration1570087128.Migrate,
+			ID:      "1568390387",
+			Migrate: migration1568390387.Migrate,
 		},
 	}
 
