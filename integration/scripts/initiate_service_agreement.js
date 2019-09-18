c65dd71435d4284c3f2ecd79646242c2cf34990b
Steve Ellis <email@steveell.is>
2019-09-18 12:00:33 -0400

    WIP

--- integration/scripts/initiate_service_agreement.js
+++ integration/scripts/initiate_service_agreement.js
@@ -2,20 +2,21 @@
 
 // truffle script
 
-console.log("###########", process.env)
+//console.log("###########", process.env)
 const abi = require('ethereumjs-abi')
+global.web3 = require('web3')
 
-const chainLink = require('../evm/dist/src/helpers.js')
+const chainLink = require('../../evm/dist/src/helpers.js')
 
 const fs = require('fs')
 const request = require('request-promise').defaults({ jar: true })
 const url = require('url')
 const { abort, DEVNET_ADDRESS, scriptRunner } = require('../common.js')
 
-const CoordinatorABI = JSON.parse(fs.readFileSync('../evm/v0.5/build/contracts/Coordinator.json', 'utf8'))
+const CoordinatorABI = JSON.parse(fs.readFileSync('../../evm/v0.5/build/contracts/Coordinator.json', 'utf8'))
 const coordinatorAddress = process.env.COORDINATOR_ADDRESS
 console.log("!!!!!!!!!!!!!!!", coordinatorAddress)
-const Coordinator = web3.eth.Contracts(CoordinatorABI, coordinatorAddress)
+const Coordinator = new web3.eth.Contract(CoordinatorABI, coordinatorAddress)
 console.log("@@@@@@@@@@@@@", Coordinator)
 
 const agreement = JSON.parse(fs.readFileSync('./agreement.json', 'utf8'))
