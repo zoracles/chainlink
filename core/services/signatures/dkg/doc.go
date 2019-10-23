5b11f659d29a0ecc4b6f00c0e9c9f6e8f7ec8903
Alex Coventry <alx@mit.edu>
2019-10-23 11:08:20 -0400

    Design notes for DKG networking

--- core/services/signatures/dkg/doc.go
+++ core/services/signatures/dkg/doc.go
@@ -1,3 +1,17 @@
 // Package collaboration contains network logic for making a threshold signature
 //
-// 
+// Intended usage
+//
+// This is to be used once a consensus has developed that a message should be
+// signed. Establishing that consensus is out of scope, here, and will probably
+// involve application-specific logic.
+//
+// The nodes find each other through a DHT. Chainlink can operate as bootstrap
+// nodes for it, but other people can offer this service if they wish.
+// The
+
+// Notes
+//
+// What if we used a hierarchical deterministical wallet to generate the
+// polynomial coefficients?
+
