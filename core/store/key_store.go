4e5edfa1bf7fb29e0c82f6e4bb36ce7273a9eed4
Alex Coventry <alx@mit.edu>
2019-10-27 21:03:27 -0400

    Extract explicit private key while unlocking ethereum account

--- core/store/key_store.go
+++ core/store/key_store.go
@@ -1,13 +1,20 @@
 package store
 
 import (
-	"errors"
+	"crypto/ecdsa"
 	"fmt"
+	"io/ioutil"
 	"math/big"
+	"path/filepath"
+
+	"github.com/pkg/errors"
+	"github.com/tidwall/gjson"
 
 	"github.com/ethereum/go-ethereum/accounts"
 	"github.com/ethereum/go-ethereum/accounts/keystore"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/smartcontractkit/chainlink/core/logger"
 	"github.com/smartcontractkit/chainlink/core/store/models"
 	"github.com/smartcontractkit/chainlink/core/utils"
@@ -17,6 +24,8 @@ import (
 // KeyStore manages a key storage directory on disk.
 type KeyStore struct {
 	*keystore.KeyStore
+	keysDir  string // Path to directory containing ethereum key material
+	PrivKeys map[common.Address]*ecdsa.PrivateKey
 }
 
 // NewKeyStore creates a keystore for the given directory.
@@ -26,8 +35,7 @@ func NewKeyStore(keyDir string) *KeyStore {
 		keystore.StandardScryptN,
 		keystore.StandardScryptP,
 	)
-
-	return &KeyStore{ks}
+	return &KeyStore{ks, keyDir, make(map[common.Address]*ecdsa.PrivateKey)}
 }
 
 // HasAccounts returns true if there are accounts located at the keystore
@@ -36,8 +44,36 @@ func (ks *KeyStore) HasAccounts() bool {
 	return len(ks.Accounts()) > 0
 }
 
+// privateKey searches ks.keysDir for key material matching account a, and
+// attempts to decrypt it and return the corresponding private key.
+func (ks *KeyStore) keyDataFor(a accounts.Account, phrase string) (*ecdsa.PrivateKey, error) {
+	keyFiles, err := ioutil.ReadDir(ks.keysDir)
+	if err != nil {
+		return nil, errors.Wrapf(err, "while trying to list key files in %s", ks.keysDir)
+	}
+	for _, file := range keyFiles {
+		fullPath := filepath.Join(ks.keysDir, file.Name())
+		contents, err := ioutil.ReadFile(fullPath)
+		if err != nil {
+			return nil, errors.Wrapf(err, "while trying to read keyfile %s", fullPath)
+		}
+		if a.Address == common.HexToAddress(gjson.Get(string(contents), "address").String()) {
+			key, err := keystore.DecryptKey(contents, phrase)
+			if err != nil {
+				return nil, errors.Wrapf(err, "while decrypting keydata")
+			}
+			if a.Address != crypto.PubkeyToAddress(key.PrivateKey.PublicKey) {
+				return nil, fmt.Errorf("purported key material for %s has wrong address", a.Address)
+			}
+			return key.PrivateKey, nil
+		}
+	}
+	return nil, fmt.Errorf("failed to find key file for %s", a.Address)
+}
+
 // Unlock uses the given password to try to unlock accounts located in the
-// keystore directory.
+// keystore directory, and recover their private keys to the KeyStore
+// PrivateKeys map.
 func (ks *KeyStore) Unlock(phrase string) error {
 	var merr error
 	for _, account := range ks.Accounts() {
@@ -47,6 +83,11 @@ func (ks *KeyStore) Unlock(phrase string) error {
 		} else {
 			logger.Infow(fmt.Sprint("Unlocked account ", account.Address.Hex()), "address", account.Address.Hex())
 		}
+		key, err := ks.keyDataFor(account, phrase)
+		if err != nil {
+			return errors.Wrapf(err, "while retrieving key material")
+		}
+		ks.PrivKeys[account.Address] = key
 	}
 	return merr
 }
